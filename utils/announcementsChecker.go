efa7372f10336f9fab47cf8dfb09f44c18cbca30
Tanish2002 <tanishkhare@gmail.com>
2020-10-13 08:20:28 +0530

    Use Brain and do efficient coding :)

--- utils/announcementsChecker.go
+++ utils/announcementsChecker.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -10,244 +11,43 @@ import (
 )
 
 var (
-	BEE             string
-	YOGA            string
-	SEMI_CONDUCTORS string
-	ENGLISH         string
-	PROFFESIONAL    string
-	EG              string
-	MATHS           string
-	CONSTITUITION   string
-	TEST            string
-	CHANNEL         string = os.Getenv("CHANNEL")
+	CHANNEL string = os.Getenv("CHANNEL")
 )
 
 func AnnouncementCheck(s *discordgo.Session, sr *classroom.Service) {
-	r, err := sr.Courses.List().PageSize(100).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve courses. %v", err)
+	ids := strings.Split(os.Getenv("COURSE_ID"), ",")
+	names := strings.Split(os.Getenv("COURSE_NAME"), ",")
+	if len(ids) != len(names) {
+		log.Fatalf("no of Course_ID != no of Course_Name")
+		return
+	}
+	courses := make([]*classroom.Course, len(ids))
+	for i, id := range ids {
+		course := &classroom.Course{Id: id, Name: names[i]}
+		courses[i] = course
 	}
 	for {
-		if len(r.Courses) > 0 {
-			for _, c := range r.Courses {
-				t, err := sr.Courses.Teachers.List(c.Id).PageSize(1).Do()
-				if err != nil {
-					log.Fatalf("Unable to retrieve Teacher Details. %s", err)
+		for _, cs := range courses {
+			cs := cs
+			l, err := sr.Courses.Announcements.List(cs.Id).PageSize(1).Do()
+			if err != nil {
+				log.Fatalf("Unable to retrieve Announcements for %s. %v", cs.Name, err)
+			}
+			for _, a := range l.Announcements {
+				if len(cs.AlternateLink) == 0 {
+					cs.AlternateLink = a.AlternateLink
 				}
-				for _, teacher := range t.Teachers {
-					switch c.Id {
-					case "161832138169":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(YOGA) == 0 {
-								YOGA = a.AlternateLink
-							}
-							if YOGA != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for YOGA CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								YOGA = a.AlternateLink
-							}
-						}
-					case "167939178573":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(CONSTITUITION) == 0 {
-								CONSTITUITION = a.AlternateLink
-							}
-							if CONSTITUITION != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for CONSTITUITION OF INDIA CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								CONSTITUITION = a.AlternateLink
-							}
-						}
-
-					case "169440007943":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(BEE) == 0 {
-								BEE = a.AlternateLink
-							}
-							if BEE != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for BEE CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								BEE = a.AlternateLink
-							}
-						}
-
-					case "56523552967":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(PROFFESIONAL) == 0 {
-								PROFFESIONAL = a.AlternateLink
-							}
-							if PROFFESIONAL != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for PROFESSIONAL CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								PROFFESIONAL = a.AlternateLink
-							}
-						}
-
-					case "167609056736":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(ENGLISH) == 0 {
-								ENGLISH = a.AlternateLink
-							}
-							if ENGLISH != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for ENGLISH CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								ENGLISH = a.AlternateLink
-							}
-						}
-
-					case "145573038389":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(EG) == 0 {
-								EG = a.AlternateLink
-							}
-							if EG != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for ENGINEERING GRAPHICS CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								EG = a.AlternateLink
-							}
-						}
-
-					case "161369689917":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(SEMI_CONDUCTORS) == 0 {
-								SEMI_CONDUCTORS = a.AlternateLink
-							}
-							if SEMI_CONDUCTORS != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for SEMICONDUCTORS CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								SEMI_CONDUCTORS = a.AlternateLink
-							}
-						}
-
-					case "161968063971":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(MATHS) == 0 {
-								MATHS = a.AlternateLink
-							}
-							if MATHS != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for MATHS CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								MATHS = a.AlternateLink
-							}
-						}
-
-					case "150436902293":
-						l, err := sr.Courses.Announcements.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Announcements for %s. %v", c.Name, err)
-						}
-						for _, a := range l.Announcements {
-							if len(TEST) == 0 {
-								TEST = a.AlternateLink
-							}
-							if TEST != a.AlternateLink {
-								s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
-									Title:       "New Announcement for TEST CLASS",
-									URL:         a.AlternateLink,
-									Description: a.Text,
-									Timestamp:   a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								})
-								TEST = a.AlternateLink
-							}
-						}
-					}
+				if cs.AlternateLink != a.AlternateLink {
+					s.ChannelMessageSendEmbed(CHANNEL, &discordgo.MessageEmbed{
+						Title:       "New Announcement for " + cs.Name + " CLASS",
+						URL:         a.AlternateLink,
+						Description: a.Text,
+						Timestamp:   a.CreationTime,
+					})
+					cs.AlternateLink = a.AlternateLink
 				}
 			}
-		} else {
-			log.Fatalf("No courses found when checking for announcements")
-			return
 		}
-		time.Sleep(10 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
--- utils/assignmentChecker.go
+++ utils/assignmentChecker.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"fmt"
 	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -11,442 +13,62 @@ import (
 
 const inline = false
 
-var (
-	BEE_A             string
-	YOGA_A            string
-	SEMI_CONDUCTORS_A string
-	ENGLISH_A         string
-	PROFFESIONAL_A    string
-	EG_A              string
-	MATHS_A           string
-	CONSTITUITION_A   string
-	TEST_A            string
-)
-
 func AssignmentCheck(s *discordgo.Session, sr *classroom.Service) {
-	r, err := sr.Courses.List().PageSize(100).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve courses. %v", err)
+	ids := strings.Split(os.Getenv("COURSE_ID"), ",")
+	names := strings.Split(os.Getenv("COURSE_NAME"), ",")
+	if len(ids) != len(names) {
+		log.Fatalf("no of Course_ID != no of Course_Name")
+		return
+	}
+	courses := make([]*classroom.Course, len(ids))
+	for i, id := range ids {
+		course := &classroom.Course{Id: id, Name: names[i]}
+		courses[i] = course
 	}
 	for {
-		if len(r.Courses) > 0 {
-			for _, c := range r.Courses {
-				t, err := sr.Courses.Teachers.List(c.Id).PageSize(1).Do()
-				if err != nil {
-					log.Fatalf("Unable to retrieve Teacher Details. %s", err)
+		for _, cs := range courses {
+			cs := cs
+			l, err := sr.Courses.CourseWork.List(cs.Id).PageSize(1).Do()
+			if err != nil {
+				log.Fatalf("Unable to retrieve Announcements for %s. %v", cs.Name, err)
+			}
+			for _, a := range l.CourseWork {
+				if len(cs.AlternateLink) == 0 {
+					cs.AlternateLink = a.AlternateLink
 				}
-				for _, teacher := range t.Teachers {
-					switch c.Id {
-					case "161832138169":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-						for _, a := range l.CourseWork {
-							if len(YOGA_A) == 0 {
-								YOGA_A = a.AlternateLink
-							}
-							if YOGA_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for YOGA CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								YOGA_A = a.AlternateLink
-							}
-						}
-					case "167939178573":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-						for _, a := range l.CourseWork {
-							if len(CONSTITUITION_A) == 0 {
-								CONSTITUITION_A = a.AlternateLink
-							}
-							if CONSTITUITION_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for CONSTITUITION OF INDIA CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								CONSTITUITION_A = a.AlternateLink
-							}
-						}
-					case "169440007943":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-						for _, a := range l.CourseWork {
-							if len(BEE_A) == 0 {
-								BEE_A = a.AlternateLink
-							}
-							if BEE_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for BEE CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								BEE_A = a.AlternateLink
-							}
-						}
-					case "56523552967":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-						for _, a := range l.CourseWork {
-							if len(PROFFESIONAL_A) == 0 {
-								PROFFESIONAL_A = a.AlternateLink
-							}
-							if PROFFESIONAL_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for PROFESSIONAL SKILLS CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								PROFFESIONAL_A = a.AlternateLink
-							}
-						}
-					case "167609056736":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-
-						for _, a := range l.CourseWork {
-							if len(ENGLISH_A) == 0 {
-								ENGLISH_A = a.AlternateLink
-							}
-							if ENGLISH_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for ENGLISH CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								ENGLISH_A = a.AlternateLink
-							}
-						}
-					case "145573038389":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-
-						for _, a := range l.CourseWork {
-							if len(EG_A) == 0 {
-								EG_A = a.AlternateLink
-							}
-							if EG_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for ENGINEERING GRAPHICS CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								EG_A = a.AlternateLink
-							}
-						}
-					case "161369689917":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-
-						for _, a := range l.CourseWork {
-							if len(SEMI_CONDUCTORS_A) == 0 {
-								SEMI_CONDUCTORS_A = a.AlternateLink
-							}
-							if SEMI_CONDUCTORS_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for SEMI CONDUCTORS PHYSICS CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								SEMI_CONDUCTORS_A = a.AlternateLink
-							}
-						}
-					case "161968063971":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-
-						for _, a := range l.CourseWork {
-							if len(MATHS_A) == 0 {
-								MATHS_A = a.AlternateLink
-							}
-							if MATHS_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for MATHS CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								MATHS_A = a.AlternateLink
-							}
-						}
-					case "150436902293":
-						l, err := sr.Courses.CourseWork.List(c.Id).PageSize(1).Do()
-						if err != nil {
-							log.Fatalf("Unable to retrieve Assignments for %s. %v", c.Name, err)
-						}
-						for _, a := range l.CourseWork {
-							if len(TEST_A) == 0 {
-								TEST_A = a.AlternateLink
-							}
-							if TEST_A != a.AlternateLink {
-								embed := discordgo.MessageEmbed{
-									Title:     "New Assignment for TEST CLASS",
-									URL:       a.AlternateLink,
-									Fields:    []*discordgo.MessageEmbedField{},
-									Timestamp: a.CreationTime,
-									Footer: &discordgo.MessageEmbedFooter{
-										Text: teacher.Profile.Name.FullName,
-									},
-								}
-								if len(a.Title) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Topic",
-										Value:  a.Title,
-										Inline: inline,
-									})
-								}
-								if len(a.Description) != 0 {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Description",
-										Value:  a.Description,
-										Inline: inline,
-									})
-								}
-								if a.DueDate != nil {
-									embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
-										Name:   "Due Date",
-										Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
-										Inline: inline,
-									})
-								}
-								s.ChannelMessageSendEmbed(CHANNEL, &embed)
-								TEST_A = a.AlternateLink
-							}
-						}
+				if cs.AlternateLink != a.AlternateLink {
+					embed := discordgo.MessageEmbed{
+						Title:     "New Assignment for " + cs.Name + " CLASS",
+						URL:       a.AlternateLink,
+						Fields:    []*discordgo.MessageEmbedField{},
+						Timestamp: a.CreationTime,
 					}
+					if len(a.Title) != 0 {
+						embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+							Name:   "Topic",
+							Value:  a.Title,
+							Inline: inline,
+						})
+					}
+					if len(a.Description) != 0 {
+						embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+							Name:   "Description",
+							Value:  a.Description,
+							Inline: inline,
+						})
+					}
+					if a.DueDate != nil {
+						embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+							Name:   "Due Date",
+							Value:  fmt.Sprintf("%d/%d/%d", a.DueDate.Day, a.DueDate.Month, a.DueDate.Year),
+							Inline: inline,
+						})
+					}
+					s.ChannelMessageSendEmbed(CHANNEL, &embed)
+					cs.AlternateLink = a.AlternateLink
 				}
 			}
-		} else {
-			log.Fatalf("No courses found when checking for assigments")
-			return
 		}
-		time.Sleep(10 * time.Second)
+		time.Sleep(1 * time.Second)
 	}
 }
